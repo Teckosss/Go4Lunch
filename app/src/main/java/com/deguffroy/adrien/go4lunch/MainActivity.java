fad591685c99f291a659369a0ffd1e493c503e5d
Adrien Deguffroy <adrien-deg@hotmail.fr>
2018-07-13 05:48:18 +0200

    Adding NavigationDrawer / Retrieve user info from Firestore / Logout / Adding Glide & Crashlytic

--- app/src/main/java/com/deguffroy/adrien/go4lunch/MainActivity.java
+++ app/src/main/java/com/deguffroy/adrien/go4lunch/MainActivity.java
@@ -1,45 +1,74 @@
 package com.deguffroy.adrien.go4lunch;
 
 import android.content.Intent;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.request.RequestOptions;
 import com.deguffroy.adrien.go4lunch.Api.UserHelper;
+import com.deguffroy.adrien.go4lunch.Models.User;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.GrayscaleTransformation;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    @BindView(R.id.activity_main_drawer_layout) DrawerLayout mDrawerLayout;
     @BindView(R.id.bottom_navigation) BottomNavigationView mBottomNavigationView;
     @BindView(R.id.simple_toolbar) Toolbar toolbar;
+    @BindView(R.id.activity_main_nav_view) NavigationView mNavigationView;
 
     //FOR DATA
     private static final int RC_SIGN_IN = 123;
+    private static final int SIGN_OUT_TASK = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        this.startSignInActivity();
 
+        if (!this.isCurrentUserLogged()){
+            this.startSignInActivity();
+        }
+
+        this.updateUIWhenCreating();
+
+        this.configureNavigationView();
         this.configureToolBar();
+        this.configureDrawerLayout();
         this.configureBottomView();
     }
 
@@ -59,9 +88,36 @@ public class MainActivity extends AppCompatActivity {
                                 Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build(),      // GOOGLE
                                         new AuthUI.IdpConfig.FacebookBuilder().build()))         // FACEBOOK
                         .setIsSmartLockEnabled(false, true)
-                        .setLogo(R.drawable.ic_logo_auth)
+                        .setLogo(R.drawable.meal)
                         .build(),
                 RC_SIGN_IN);
+    }
+
+    // ---------------------
+    // ACTIONS
+    // ---------------------
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // Handle Navigation Item Click
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.drawer_lunch :
+                break;
+            case R.id.drawer_settings:
+                break;
+            case R.id.drawer_logout:
+                this.signOutUserFromFirebase();
+                break;
+            default:
+                break;
+        }
+
+        this.mDrawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     // ---------------------
@@ -73,11 +129,33 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    // Configure Drawer Layout
+    private void configureDrawerLayout(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // Configure NavigationView
+    private void configureNavigationView(){
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu and add it to the Toolbar
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Handle back click to close menu
+        if (this.mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void configureBottomView(){
@@ -90,12 +168,15 @@ public class MainActivity extends AppCompatActivity {
 
                             case R.id.map:
                                 //Action quand onglet 1 sélectionné
+                                Toast.makeText(MainActivity.this, "Click on Map", Toast.LENGTH_SHORT).show();
                                 break;
                             case R.id.list:
                                 //Action quand onglet 2 sélectionné
+                                Toast.makeText(MainActivity.this, "Click on List", Toast.LENGTH_SHORT).show();
                                 break;
                             case R.id.mates:
                                 //Action quand onglet 3 sélectionné
+                                Toast.makeText(MainActivity.this, "Click on Mates", Toast.LENGTH_SHORT).show();
                                 break;
                         }
                         return true;
@@ -104,10 +185,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // --------------------
+    // UI
+    // --------------------
+
+    // 1 - Update UI when activity is creating
+    private void updateUIWhenCreating(){
+
+        if (this.getCurrentUser() != null){
+            View headerContainer = mNavigationView.getHeaderView(0); // This returns the container layout in nav_drawer_header.xml (e.g., your RelativeLayout or LinearLayout)
+            ImageView mImageView = headerContainer.findViewById(R.id.drawer_image);
+            ImageView mImageView_bk = headerContainer.findViewById(R.id.drawer_image_bk);
+            TextView mNameText = headerContainer.findViewById(R.id.drawer_name);
+            TextView mEmailText = headerContainer.findViewById(R.id.drawer_email);
+
+            Glide.with(this)
+                    .load(R.drawable.lunch)
+                    .apply(RequestOptions.bitmapTransform(new BlurTransformation(40)))
+                    .into(mImageView_bk);
+
+            ColorMatrix colorMatrix = new ColorMatrix();
+            colorMatrix.setSaturation(.15f);
+            ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
+            mImageView_bk.setColorFilter(filter);
+
+            //Get picture URL from Firebase
+            if (this.getCurrentUser().getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(this.getCurrentUser().getPhotoUrl())
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(mImageView);
+            }
+
+            //Get email from Firebase
+            String email = TextUtils.isEmpty(this.getCurrentUser().getEmail()) ? getString(R.string.info_no_email_found) : this.getCurrentUser().getEmail();
+            String name = TextUtils.isEmpty(this.getCurrentUser().getDisplayName()) ? getString(R.string.info_no_username_found) : this.getCurrentUser().getDisplayName();
+
+            //Update views with data
+            mEmailText.setText(email);
+            mNameText.setText(name);
+        }
+    }
+
+    // Create OnCompleteListener called after tasks ended
+    private OnSuccessListener<Void> updateUIAfterRESTRequestsCompleted(final int origin){
+        return new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                switch (origin){
+                    case SIGN_OUT_TASK:
+                        finish();
+                        startSignInActivity();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+    }
+
+    // --------------------
     // REST REQUEST
     // --------------------
 
-    // 1 - Http request that create user in firestore
+    // Http request that create user in firestore
     private void createUserInFirestore(){
 
         if (this.getCurrentUser() != null){
@@ -118,6 +258,12 @@ public class MainActivity extends AppCompatActivity {
 
             UserHelper.createUser(uid, username, urlPicture).addOnFailureListener(this.onFailureListener());
         }
+    }
+
+    private void signOutUserFromFirebase(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted(SIGN_OUT_TASK));
     }
 
     // --------------------
@@ -149,6 +295,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == RC_SIGN_IN) {
             if (resultCode == RESULT_OK) { // SUCCESS
                 this.createUserInFirestore();
+                this.updateUIWhenCreating();
             } else { // ERRORS
                 if (response == null) {
                     Toast.makeText(this, getString(R.string.error_authentication_canceled), Toast.LENGTH_SHORT).show();
